ac1178d5b64d008e402074abc33dbf18fe323595
kyliepost <kylie.postlethwaite00@gmail.com>
2021-07-22 15:29:57 -0500

    debugging take 2

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,12 +1,28 @@
 import { getProducts } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [, productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt()) {
+                    window.alert(` ${product.na} costs ${product.price} `)
+                }
+            }
+        }
+    }
+)
+
 const products = getProducts()
 
 export const Products = () => {
     let html = "<ul>"
 
     for (const product of products) {
-        html += `<li id="product--${product.id}">${product.title}</li>`
+        html += `<li id="product--${product.id}">${product.name}</li>`
     }
 
     html += "</ul>"
